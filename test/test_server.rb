cf742a233ca7ff1d5b7661395027fac0e73d61c4
Ripta Pasay <github@r8y.org>
2008-12-11 04:48:38 -0500

    Implement tests for client querying

--- test/test_server.rb
+++ test/test_server.rb
@@ -41,8 +41,13 @@ class TestServer < Test::Unit::TestCase
     def inspect
       {:channels => @channels, :client_id => @client_id, :session_id => @session_id}.inspect
     end
-    def query_show_clients
-      self.transmit :command => :query, :type => :show_clients
+    def query_show_client(client_id)
+      self.transmit :command => :query, :type => :show_client, :client_id => client_id
+      self
+    end
+    def query_show_clients(client_ids = [])
+      self.transmit :command => :query, :type => :show_clients, :client_ids => client_ids
+      self
     end
     def receive
       return nil unless @socket
@@ -66,10 +71,12 @@ class TestServer < Test::Unit::TestCase
       channels.each do |channel|
         @channels << channel.to_s unless @channels.include?(channel.to_s)
       end
-      self.transmit :command => :subscribe, :client_id => @client_id, :session_id => @session_id, :channels => channels
+      self.transmit :command => :subscribe, :channels => channels
       self
     end
     def transmit(hash, wait_response = false)
+      hash[:client_id] ||= @client_id
+      hash[:session_id] ||= @session_id
       @socket.print(hash.to_json + "\0")
       @socket.flush
       if wait_response
@@ -249,6 +256,97 @@ class TestServer < Test::Unit::TestCase
           self.new_client { |c| c.broadcast_to_clients %w(broadcast_client), body }
         end
         assert_body body, subscriber
+      end
+      
+    end
+    
+    context "querying client list" do
+      
+      should "return all clients" do
+        subscriber = nil
+        with_server do
+          self.new_client(:client_id => "alex") { |c| c.subscribe %w() }
+          self.new_client(:client_id => "bob") { |c| c.subscribe %w() }
+          subscriber = self.new_client(:client_id => "cindy") { |c| c.subscribe %w(); c.query_show_clients }
+        end
+        assert_not_nil subscriber
+        result = subscriber.receive
+        assert_not_nil result
+        assert_equal 3, result.size
+        assert_equal %w(alex bob cindy), result.collect { |r| r["client_id"] }
+      end
+      
+      should "not include disconnected clients" do
+        subscriber = nil
+        with_server do
+          self.new_client(:client_id => "sandra") { |c| c.subscribe %w() }
+          self.new_client(:client_id => "tom") { |c| c.subscribe %w() }.close
+          subscriber = self.new_client(:client_id => "vivian") { |c| c.subscribe %w(); c.query_show_clients }
+        end
+        assert_not_nil subscriber
+        result = subscriber.receive
+        assert_not_nil result
+        assert_equal 2, result.size
+        assert_equal %w(sandra vivian), result.collect { |r| r["client_id"] }
+      end
+      
+      should "only return requested clients" do
+        subscriber = nil
+        with_server do
+          self.new_client(:client_id => "dixie") { |c| c.subscribe %w() }
+          self.new_client(:client_id => "eamon") { |c| c.subscribe %w() }
+          self.new_client(:client_id => "fanny") { |c| c.subscribe %w() }
+          subscriber = self.new_client(:client_id => "zelda") { |c| c.subscribe %w(); c.query_show_clients %w(dixie fanny) }
+        end
+        assert_not_nil subscriber
+        result = subscriber.receive
+        assert_not_nil result
+        assert_equal 2, result.size
+        assert_equal %w(dixie fanny), result.collect { |r| r["client_id"] }
+      end
+      
+      should "never return non-existant clients even when requested" do
+        subscriber = nil
+        with_server do
+          self.new_client(:client_id => "dixie") { |c| c.subscribe %w() }
+          self.new_client(:client_id => "eamon") { |c| c.subscribe %w() }
+          self.new_client(:client_id => "fanny") { |c| c.subscribe %w() }
+          subscriber = self.new_client(:client_id => "zelda") { |c| c.subscribe %w(); c.query_show_clients %w(ginny homer) }
+        end
+        assert_not_nil subscriber
+        result = subscriber.receive
+        assert_not_nil result
+        assert_equal 0, result.size
+      end
+      
+      should "return correct number of active connections" do
+        subscriber = nil
+        with_server do
+          5.times { self.new_client(:client_id => "homer") { |c| c.subscribe %w() } }
+          subscriber = self.new_client(:client_id => "zelda") { |c| c.subscribe %w(); c.query_show_clients %w(homer) }
+        end
+        assert_not_nil subscriber
+        result = subscriber.receive
+        assert_not_nil result
+        assert_equal 1, result.size
+        assert_equal 5, result.first["num_connections"]
+      end
+      
+      should "be equivalent when querying one client" do
+        s1, s2 = nil
+        with_server do
+          5.times { self.new_client(:client_id => "homer") { |c| c.subscribe %w() } }
+          s1 = self.new_client(:client_id => "zelda") { |c| c.subscribe %w(); c.query_show_client "homer" }
+          s2 = self.new_client(:client_id => "zelda") { |c| c.subscribe %w(); c.query_show_clients %w(homer) }
+        end
+        assert_not_nil s1
+        assert_not_nil s2
+        r1 = s1.receive
+        assert_not_nil r1
+        r2 = s2.receive
+        assert_not_nil r2
+        assert_equal 1, r2.size
+        assert_equal r1, r2.first
       end
       
     end
